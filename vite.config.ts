b2ecff1d2ffea3f4efa633fbd243c04b65ebf54e
Toby Phipps <tphipps@atsui.com>
2025-05-16 01:41:30 +1000

    Removed menu auto-hide. Added production deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/redhill-kiosk/' : '/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
